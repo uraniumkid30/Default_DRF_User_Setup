c886f9445ab651d8f3b8af42535fcff0b457092e
uraniumkid30 <uraniumkid30@gmail.com>
2023-07-30 06:41:20 +0100

    Added drf-spectacular lib

--- conf/urls.py
+++ conf/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -11,7 +15,11 @@ admin.autodiscover()
 app = "applications."
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(f"api/{settings.API_VERSION}/", include(f'{app}urls'))
+    path(f"api/{settings.API_VERSION}/", include(f'{app}urls', namespace="apis")),
+    path(f'api/{settings.API_VERSION}/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # # Optional UI:
+    path(f'api/{settings.API_VERSION}/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name="schema"), name='swagger-ui'),
+    path(f'api/{settings.API_VERSION}/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
